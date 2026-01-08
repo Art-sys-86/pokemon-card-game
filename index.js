aaa5c006c2ba427b6f8a510e2069b0e10a0e1527
Art-sys-86 <hithukten@gmail.com>
2026-01-08 21:01:41 +0600

    Comments added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const img = document.getElementById('img');
 const audio = document.getElementById('pokemon-sound');
+
+
 async function getPokemon() {
 const search = document.getElementById('search').value.toLowerCase();
 const name = document.getElementById('name');
@@ -9,24 +11,23 @@ const abilityOne = document.getElementById('a1');
 const abilityTwo = document.getElementById('a2');
 
     try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
-        const data = await response.json();
-        console.log(data);
-        const pokemonName = data.name;
-        const sprite = data.sprites.front_default;
-        const cry = data.cries.latest;
-        audio.src = cry;
-        name.textContent = pokemonName.toUpperCase();
-        weight.textContent = `Weight: ${data.weight}`;
-        height.textContent = `Height: ${data.height}`;
-        abilityOne.textContent =`-> ${data.abilities[0].ability.name}`;
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`); //Fetch data from API
+        const data = await response.json(); //Convert it to json
+        const pokemonName = data.name; //Fetch the name data
+        const sprite = data.sprites.front_default; //change the image source to the api image
+        const cry = data.cries.latest; //change the audio source to the api source
+        audio.src = cry; //assign to audio
+        name.textContent = pokemonName.toUpperCase(); //change the name from html to api name value
+        weight.textContent = `Weight: ${data.weight}`; //Change the weight to api wweight
+        height.textContent = `Height: ${data.height}`; //change the height to api height
+        abilityOne.textContent =`-> ${data.abilities[0].ability.name}`; 
         abilityTwo.textContent = `-> ${data.abilities[1].ability.name}`;
-        img.src = sprite;
+        img.src = sprite; //assign the image src.
     } catch (error) {
         console.error(error);
     }
 }
 
-function playAudio(){
+function playAudio(){ //plays the audio on click
     audio.play();
 }
