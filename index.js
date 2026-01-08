5ab256af81b27ca8913072b7acc66d37f16ce1f4
Art-sys-86 <hithukten@gmail.com>
2026-01-08 20:48:56 +0600

    Finished! Version 1
    Added some improvement and fixed js logics. API are fun!

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const enter = document.getElementById('result');
 const img = document.getElementById('img');
-
+const weight = document.getElementById('weight');
+const height = document.getElementById('height');
+const abilityOne = document.getElementById('a1');
+const abilityTwo = document.getElementById('a2');
 
 async function getPokemonName() {
 const search = document.getElementById('search').value;
+const name = document.getElementById('name').value;
     try {
-        
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
         const data = await response.json();
         const name = data.name;
         const capitalized = name.charAt(0).toUpperCase()+name.slice(1);
         const sprite = data.sprites.front_default;
-        enter.textContent = (`The name of the Pokemon is ${capitalized}`);
+        name.textContent = `${capitalized}`;
         img.src = sprite;
     } catch (error) {
         console.error(error);
---
+++ readme.md
@@ -0,0 +1,4 @@
+Play Icon: https://www.freeiconspng.com/img/18927
+Back card: https://www.freeiconspng.com/img/18174
+Flip Card Tutorial: https://www.youtube.com/watch?v=QON4dFDzsiE
+Pokeball: https://www.flaticon.com/free-icons/pokemon
